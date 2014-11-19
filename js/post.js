63bb9d7e426f631445827d7526a7ee760c5a0ed6
BeiYuu <dewreg@gmail.com>
2014-11-19 18:07:50 +0800

    delete disqus

--- js/post.js
+++ js/post.js
@@ -23,17 +23,6 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
-    //***********************
-    //**评论的代码也删掉哦***
-    window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
-    //**评论的代码也删掉哦***
-    //***********************
-
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
         if(href){
